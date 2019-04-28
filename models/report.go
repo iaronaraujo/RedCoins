f198a49c4c935766095e039dfba136e390ad2755
iaronaraujo <iaron.araujo@ccc.ufcg.edu.br>
2019-04-28 12:49:22 -0300

    renamed controller, created report model

---
+++ models/report.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"time"
+
+	"github.com/iaronaraujo/RedCoins/lib"
+)
+
+//TransactionType represents the type of a transaction
+type TransactionType string
+
+const (
+	//BuyBitCoins represents the type related to bit coins buy order
+	BuyBitCoins TransactionType = "Compra de BC"
+
+	//SellBitCoins represents the type related to bit coins sell order
+	SellBitCoins TransactionType = "Venda de BC"
+)
+
+//Report represents a transaction report in redcoins
+type Report struct {
+	ID              int             `db:"id" json:"id"`
+	Transaction     TransactionType `db:"transaction" json:"transaction"`
+	BitCoins        float32         `db:"bit_coins" json:"bit_coins"`
+	Reais           float32         `db:"reais" json:"reais"`
+	TransactionDate time.Time       `db:"transaction_date" json:"transaction_date"`
+	UserID 			int             `db:"user_id" json:"user_id"`
+}
+
+//ReportModel receives the Database table
+var ReportModel = lib.Sess.Collection("reports")
