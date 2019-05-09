e9cb8d15e73384b29b2b4c1a113adf157eded1b2
iaronaraujo <iaron.araujo@ccc.ufcg.edu.br>
2019-05-09 14:49:48 -0300

    removed middleware

--- app.go
+++ app.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	r "github.com/iaronaraujo/RedCoins/routers"
-	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := r.App
-	e.Use(middleware.Logger())
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Start(":3000")
 }
